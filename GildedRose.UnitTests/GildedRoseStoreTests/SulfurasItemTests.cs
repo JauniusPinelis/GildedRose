261c139d4797f5403890723edab6d47ebec9cec8
jaunius <jaunius.pinelis@gmail.com>
2019-09-22 16:35:30 +0300

    Added Tests for Sulfuras Item

---
+++ GildedRose.UnitTests/GildedRoseStoreTests/SulfurasItemTests.cs
@@ -0,0 +1,49 @@
+﻿using GildedRose.Business;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GildedRose.UnitTests.GildedRoseStoreTests
+{
+    public class SulfurasItemTests
+    {
+        private GildedRoseStore _gildedRose;
+
+        [Fact]
+        public void UpdateQuality_GivenSulfurasItem_QualityShouldNotChange()
+        {
+            _gildedRose = new GildedRoseStore(new List<Item>()
+            {
+               new Item()
+                {
+                    Name = "Sulfuras, Hand of Ragnaros",
+                    Quality = 80,
+                    SellIn = 10
+                }
+            });
+
+            _gildedRose.UpdateQuality();
+
+            Assert.Equal(80, _gildedRose.GetItems()[0].Quality);
+        }
+
+        [Fact]
+        public void UpdateQuality_GivenSulfurasItem_SellInShouldNotChange()
+        {
+            _gildedRose = new GildedRoseStore(new List<Item>()
+            {
+               new Item()
+                {
+                    Name = "Sulfuras, Hand of Ragnaros",
+                    Quality = 80,
+                    SellIn = 10
+                }
+            });
+
+            _gildedRose.UpdateQuality();
+
+            Assert.Equal(10, _gildedRose.GetItems()[0].SellIn);
+        }
+    }
+}
