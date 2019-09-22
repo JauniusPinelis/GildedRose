c1ad1c547ab26cf62ca6f8e4ab7caee6214d6278
jaunius <jaunius.pinelis@gmail.com>
2019-09-22 19:15:06 +0300

    Added Unit tests for Conjured Item

---
+++ GildedRose.UnitTests/GildedRoseStoreTests/ConjuredItemTests.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GildedRose.Business.Strategies
+{
+    public class ConjuredItemTests
+    {
+        private GildedRoseStore _gildedRose;
+
+        
+
+        [Fact]
+        public void UpdateQuality_GivenConjuredItem_QualityShouldBeMinusTwo()
+        {
+
+            _gildedRose = new GildedRoseStore(new List<Item>()
+            {
+               new Item()
+                {
+                    Name = "Conjured Mana Cake",
+                    Quality = 20,
+                    SellIn = 10
+                }
+            });
+
+            _gildedRose.UpdateQuality();
+
+            Assert.Equal(18, _gildedRose.GetItems()[0].Quality);
+        }
+
+        [Fact]
+        public void UpdateQuality_GivenConjuredItem_SellInShouldBeMinusOne()
+        {
+
+            _gildedRose = new GildedRoseStore(new List<Item>()
+            {
+               new Item()
+                {
+                    Name = "Conjured Mana Cake",
+                    Quality = 20,
+                    SellIn = 10
+                }
+            });
+
+            _gildedRose.UpdateQuality();
+
+            Assert.Equal(9, _gildedRose.GetItems()[0].SellIn);
+        }
+
+        [Fact]
+        public void UpdateQuality_GivenConjuredItem_QualityDoesNotGoBelowZero()
+        {
+
+            _gildedRose = new GildedRoseStore(new List<Item>()
+            {
+               new Item()
+                {
+                    Name = "Conjured Mana Cake",
+                    Quality = 0,
+                    SellIn = 10
+                }
+            });
+
+            _gildedRose.UpdateQuality();
+
+            Assert.Equal(0, _gildedRose.GetItems()[0].Quality);
+        }
+
+        [Fact]
+        public void UpdateQuality_GivenConjuredItemRanTwice_SellInIsNegative()
+        {
+
+            _gildedRose = new GildedRoseStore(new List<Item>()
+            {
+               new Item()
+                {
+                    Name = "Conjured Mana Cake",
+                    Quality = 10,
+                    SellIn = 1
+                }
+            });
+
+            _gildedRose.UpdateQuality();
+            _gildedRose.UpdateQuality();
+
+            Assert.Equal(-1, _gildedRose.GetItems()[0].SellIn);
+        }
+
+        [Fact]
+        public void UpdateQuality_GivenConjuredItemRanThreeTimes_QualityDecreaseisQuadripledWhenSellInIsNegative()
+        {
+
+            _gildedRose = new GildedRoseStore(new List<Item>()
+            {
+               new Item()
+                {
+                    Name = "Conjured Mana Cake",
+                    Quality = 20,
+                    SellIn = 1
+                }
+            });
+
+            _gildedRose.UpdateQuality();
+            _gildedRose.UpdateQuality();
+            _gildedRose.UpdateQuality();
+
+            Assert.Equal(10, _gildedRose.GetItems()[0].Quality);
+        }
+
+        
+    }
+}
